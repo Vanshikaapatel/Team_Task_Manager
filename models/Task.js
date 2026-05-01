4ad8de63d07b5d7a3ae81cc92abf550d245dddc9
Vanshikaapatel <patelvanshikaa@gmail.com>
2026-05-02 02:11:19 +0530

    fixed schema error

--- models/Task.js
+++ models/Task.js
@@ -1,35 +1,21 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    dueDate: {
-        type: String
-    },
-    priority: {
-        type: String,
-        default: "Low"
-    },
+const taskSchema = new mongoose.Schema(
+{
+    title: String,
+    description: String,
+    dueDate: String,
+    priority: String,
     status: {
         type: String,
         default: "To Do"
     },
-    assignedTo: {
-        type: String,
-        required: true
-    },
-    assignedUser: {
-        type: String,
-        default: null
-    }, 
-
-    timestamps: true   // createdAt & updatedAt automatically add honge
+    assignedTo: String,
+    assignedUser: String,
+    projectId: String
+},
+{
+    timestamps: true   // 👈 YAHAN hona chahiye
 });
 
 module.exports = mongoose.model("Task", taskSchema);
